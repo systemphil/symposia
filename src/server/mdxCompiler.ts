ed45850b7d1212c25eec97e51eb3f0f11438bc27
Firgrep <awdeegrays@gmail.com>
2023-09-26 18:03:43 +0200

    mdx compiler now adds admonition classes to js output

--- src/server/mdxCompiler.ts
+++ src/server/mdxCompiler.ts
@@ -4,7 +4,7 @@ import remarkDirective from 'remark-directive';
 
 
 /**
- * Compiles strings into MDX. Configure additional plugins here.
+ * Compiles MDX strings into JavaScript. Configure additional plugins here.
  * @docs {@link https://mdxjs.com/packages/mdx/#compilefile-options | MDX compiler options}
  */
 export const mdxCompiler = async (mdxSource: string) => {
@@ -15,11 +15,79 @@ export const mdxCompiler = async (mdxSource: string) => {
             // ^-- Generate code for production.
             // `false` if you use `/jsx-runtime` on client, `true` if you use
             // `/jsx-dev-runtime`.
-            remarkPlugins: [remarkGfm, remarkDirective],
-        }))
+            remarkPlugins: [remarkGfm, remarkDirective, adminitionPlugin],
+        }));
         return mdxCompiled;
     } catch(e) {
         throw new Error("An error occured in the mdxCompiler");
     }
 }
 export type MDXCompilerReturnType = Awaited<ReturnType<typeof mdxCompiler>>;
+
+
+// Register `hName`, `hProperties` types, used when turning markdown to HTML:
+/// <reference types="mdast-util-to-hast" />
+// Register directive nodes in mdast:
+/// <reference types="mdast-util-directive" />
+
+import {h} from 'hastscript'
+import {visit} from 'unist-util-visit'
+
+const ADMONITION_TYPES = ["note", "tip", "danger", "info", "caution"]
+
+// import {Node as MDASTNode} from 'mdast-util-definitions/lib';
+
+// This plugin is an example to turn `::youtube` into iframes.
+// This plugin is an example to turn `::note` into divs, passing arbitrary
+// attributes.
+function adminitionPlugin() {
+    // TODO review library
+    // tree ought to be Node from mdast-util-definitions/lib but TS is throwing that
+    // type instantiation is excessively deep and possibly infinite.
+    // @ts-ignore
+    return (tree) => {
+        visit(tree, (node) => {
+            if (
+                node.type === 'containerDirective' ||
+                node.type === 'leafDirective' ||
+                node.type === 'textDirective'
+            ) {
+                if (ADMONITION_TYPES.includes(node.name)) {
+
+                    const data = node.data || (node.data = {})
+                    const tagName = node.type === 'textDirective' ? 'span' : 'div'
+                    
+                    if (node.name === ADMONITION_TYPES[0]) {
+                        node.attributes = {
+                            ...node.attributes,
+                            class: '_admonitionNote_13nbk_63', 
+                        };
+                    } else if (node.name === ADMONITION_TYPES[1]) {
+                        node.attributes = {
+                            ...node.attributes,
+                            class: '_admonitionTip_13nbk_63', 
+                        };
+                    } else if (node.name === ADMONITION_TYPES[2]) {
+                        node.attributes = {
+                            ...node.attributes,
+                            class: '_admonitionDanger_13nbk_63', 
+                        };
+                    } else if (node.name === ADMONITION_TYPES[3]) {
+                        node.attributes = {
+                            ...node.attributes,
+                            class: '_admonitionInfo_13nbk_63', 
+                        };
+                    } else if (node.name === ADMONITION_TYPES[4]) {
+                        node.attributes = {
+                            ...node.attributes,
+                            class: '_admonitionCaution_13nbk_63', 
+                        };
+                    }
+                    
+                    data.hName = tagName
+                    data.hProperties = h(tagName, node.attributes || {}).properties
+                }
+            }
+        })
+    }
+}
