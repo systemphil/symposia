29b2501f70bb2d11afb1439d6480bd9ae987ec2f
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-05-01 15:38:42 +0200

    feat: glow boundary

--- src/components/RootNavbar.tsx
+++ src/components/RootNavbar.tsx
@@ -14,7 +14,9 @@ export const RootNavbar = () => {
     const { data: sessionData } = useSession();
 
     return (
-        <header className={`sticky top-0 z-30 print:hidden bg-transparent ${TEXT_COLOR}`}>
+        <header
+            className={`sticky top-0 z-30 print:hidden bg-transparent ${TEXT_COLOR}`}
+        >
             <div className="'pointer-events-none absolute z-[-1] h-full w-full backdrop-blur-md bg-white/[.85] dark:!bg-slate-900/80 shadow-[0_2px_4px_rgba(0,0,0,.02),0_1px_0_rgba(0,0,0,.06)] dark:shadow-[0_-1px_0_rgba(255,255,255,.1)_inset] contrast-more:shadow-[0_0_0_1px_#000] contrast-more:dark:shadow-[0_0_0_1px_#fff]"></div>
             <div className="container ">
                 <div className="flex h-[64px] items-center justify-between py-6">
@@ -23,31 +25,51 @@ export const RootNavbar = () => {
                     */}
                     <div className={`md:hidden`}>
                         <div className="dropdown dropdown-bottom dropdown-start">
-                            <label tabIndex={1} className="btn btn-ghost btn-circle">
+                            <label
+                                tabIndex={1}
+                                className="btn btn-ghost btn-circle"
+                            >
                                 <div className="rounded-full">
-                                    <Image src="/static/svg/menu.svg" alt="" height={25} width={25}/>
+                                    <Image
+                                        src="/static/svg/menu.svg"
+                                        alt=""
+                                        height={25}
+                                        width={25}
+                                    />
                                 </div>
                             </label>
-                            <ul tabIndex={1} className="menu dropdown-content z-[1] p-2 shadow bg-white w-52 rounded-md drop-shadow-2xl">
+                            <ul
+                                tabIndex={1}
+                                className="menu dropdown-content z-[1] p-2 shadow bg-white w-52 rounded-md drop-shadow-2xl"
+                            >
                                 <li>
                                     <Link href="/">Home</Link>
-                                </li> 
+                                </li>
                                 <li>
                                     <Link href="/">Courses</Link>
                                 </li>
-                                {
-                                    sessionData && sessionData.user.role === "ADMIN" && (
+                                {sessionData &&
+                                    sessionData.user.role === "ADMIN" && (
                                         <>
                                             <li className="border-t my-1" />
                                             <li>
-                                                <Link href="/admin" className="text-purple-700">Admin</Link>
+                                                <Link
+                                                    href="/admin"
+                                                    className="text-purple-700"
+                                                >
+                                                    Admin
+                                                </Link>
                                             </li>
                                             <li>
-                                                <Link href="/test" className="text-red-500">Test</Link>
+                                                <Link
+                                                    href="/test"
+                                                    className="text-red-500"
+                                                >
+                                                    Test
+                                                </Link>
                                             </li>
                                         </>
-                                    )
-                                }
+                                    )}
                             </ul>
                         </div>
                     </div>
@@ -56,30 +78,38 @@ export const RootNavbar = () => {
                         DESKTOP MENU
                     */}
                     <div className={`hidden md:flex gap-6 md:gap-10 text-md`}>
-                        <Link href="/" className="flex items-center gap-4">
+                        <Link
+                            href="/"
+                            className="flex items-center gap-4 relative"
+                        >
                             <LogoOwl />
                             <LogoBrandAnimated />
+                            <div className="badge badge-sm badge-ghost absolute -right-4 -bottom-3 bg-pink-200 /75">
+                                Beta
+                            </div>
                         </Link>
                         <NavBarLinkDesktop href="/" text="Home" />
                         <NavBarLinkDesktop href="/courses" text="Courses" />
-                        {
-                            sessionData && sessionData.user.role === "ADMIN" && (
-                                <>
-                                    <span className="text-purple-700">
-                                        <NavBarLinkDesktop href="/admin" text="Admin" />
-                                    </span>
-                                    <span className="text-red-500">
-                                        <NavBarLinkDesktop href="/test" text="Test" />
-                                    </span>
-                                </>
-                            )
-                        }
+                        {sessionData && sessionData.user.role === "ADMIN" && (
+                            <>
+                                <span className="text-purple-700">
+                                    <NavBarLinkDesktop
+                                        href="/admin"
+                                        text="Admin"
+                                    />
+                                </span>
+                                <span className="text-red-500">
+                                    <NavBarLinkDesktop
+                                        href="/test"
+                                        text="Test"
+                                    />
+                                </span>
+                            </>
+                        )}
                     </div>
 
                     <nav className="flex gap-6 items-center">
-                        {(
-                            sessionData
-                        ) ? (
+                        {sessionData ? (
                             <UserMenu sessionData={sessionData} />
                         ) : (
                             <button
@@ -96,32 +126,39 @@ export const RootNavbar = () => {
     );
 };
 
-const NavBarLinkDesktop = ({href, text}: { href: string, text: string}) => {
-    const hoverUnderline = "after:bg-slate-700 after:absolute after:h-[2px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300";
-    const classes = "py-1 transition-colors duration-300 cursor-pointer hover:text-black dark:hover:text-white";
-
+const NavBarLinkDesktop = ({ href, text }: { href: string; text: string }) => {
+    const hoverUnderline =
+        "after:bg-slate-700 after:absolute after:h-[2px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300";
+    const classes =
+        "py-1 transition-colors duration-300 cursor-pointer hover:text-black dark:hover:text-white";
 
     return (
         <Link href={href} className="relative">
             <p className={`${cn(hoverUnderline, classes)}`}>{text}</p>
         </Link>
     );
-}
-const UserMenu = ({sessionData}: {sessionData: Session}) => {
-    return(
+};
+const UserMenu = ({ sessionData }: { sessionData: Session }) => {
+    return (
         <div className="dropdown dropdown-bottom dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
                 <div className="avatar">
                     <div className="w-12 rounded-full">
                         {/* eslint-disable-next-line */}
-                        <img 
-                            src={sessionData?.user.image ?? "/static/images/avatar_placeholder.png"} 
-                            alt="avatar_picture" 
+                        <img
+                            src={
+                                sessionData?.user.image ??
+                                "/static/images/avatar_placeholder.png"
+                            }
+                            alt="avatar_picture"
                         />
                     </div>
                 </div>
             </label>
-            <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-white rounded-box w-52 drop-shadow-2xl">
+            <ul
+                tabIndex={0}
+                className="menu dropdown-content z-[1] p-2 shadow bg-white rounded-box w-52 drop-shadow-2xl"
+            >
                 <li>
                     <a className="pointer-events-none cursor-default opacity-75 text-lg pb-0">
                         {sessionData && sessionData.user?.name}
@@ -132,13 +169,13 @@ const UserMenu = ({sessionData}: {sessionData: Session}) => {
                         {sessionData && sessionData.user?.email}
                     </a>
                 </li>
-                {sessionData && 
-                sessionData.user.provider &&
+                {sessionData && sessionData.user.provider && (
                     <li>
                         <a className="pointer-events-none cursor-default opacity-75">
                             Logged in with {sessionData.user.provider}
                         </a>
-                    </li>}
+                    </li>
+                )}
                 <li className="border-t my-1" />
                 <li>
                     <Link href="/account/billing">Billing</Link>
@@ -149,5 +186,5 @@ const UserMenu = ({sessionData}: {sessionData: Session}) => {
                 </li>
             </ul>
         </div>
-    )
-}
+    );
+};
