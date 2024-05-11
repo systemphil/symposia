456ae54d02de3039bf6a1804090a49ed0541f835
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-05-11 17:32:42 +0200

    refactor: organized home page, added descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,112 +4,128 @@ import { Reviews } from "@/components/Reviews";
 import FadeIn from "@/components/animations/FadeIn";
 import InfoCard from "@/components/InfoCard";
 import { PolyRhythmicSpiral } from "@/components/animations/PolyRhythmicSpiral";
+import Link from "next/link";
+
+// TODO add about instructors section
+// TODO add a companion to sphil section
 
 export default async function Home() {
     return (
         <>
-            <div className="h-[45rem] -my-36 md:my-6 w-full bg-white flex flex-col items-center justify-center overflow-hidden rounded-md ">
-                <div className="absolute overflow-hidden -translate-y-5">
-                    <FadeIn>
-                        <PolyRhythmicSpiral />
-                    </FadeIn>
-                </div>
-                <div className="translate-y-2 md:translate-y-1 lg:-translate-y-1 flex flex-col items-center justify-center">
-                    <div className="font-bold text-center relative z-20 bg-clip-text tracking-widest text-transparent">
-                        <Heading replacementClasses="pb-4 text-transparent bg-gradient-to-b from-black/80 to-black dark:from-white dark:to-[#AAAAAA]">
-                            Discover & Reflect
-                        </Heading>
-                    </div>
-                    <h3 className="text-xl my-1.5 md:text-2xl lg:text-3xl text-stone-600 font-light inter-var text-center">
-                        In-depth learning courses on philosophy
-                    </h3>
-                    <div className="w-[40rem] h-40 relative">
-                        {/* Gradients */}
-                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-[2px] w-3/4 blur-sm" />
-                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-px w-3/4" />
-                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-pink-400 to-transparent h-[5px] w-1/4 blur-sm" />
-                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-pink-400 to-transparent h-px w-1/4" />
-                    </div>
-                </div>
-            </div>
+            <Hero />
             <PageWrapper>
-                <FadeIn>
-                    <Heading as="h3" additionalClasses="max-w-lg">
-                        <span className="text-[#6b0072]">Symposia</span> is an
-                        online courseplatform that offers on-demand digital
-                        courses, live university-grade seminars and personalized
-                        tuition.
-                    </Heading>
-                </FadeIn>
-                <FadeIn>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full py-10">
-                        <InfoCard
-                            title="Deepen Your Thinking: Explore Philosophy In-Depth"
-                            text="Dive into the world's greatest ideas with our comprehensive online philosophy courses. Go beyond introductory lectures and engage with challenging concepts in a supportive learning environment."
-                            maskType="diamond"
-                            imgUrl="/static/images/sun.webp"
-                        />
-                        <InfoCard
-                            title="Choose Your Learning Style: Digital, with Seminars, or 1-on-1 Dialogue"
-                            text="We offer a variety of learning formats to suit your ambition. Take courses entirely on-demand with digital content at your own pace, participate in live interactive seminars with fellow peers, or get personalized guidance with private sessions from our instructors."
-                            maskType="diamond"
-                            imgUrl="/static/images/discussion.webp"
-                        />
-                        <InfoCard
-                            title="Focus on Text: Meet the Greatest Minds of History"
-                            text="Gain insights from renowned philosophers by meeting them on their own ground, namely, their text! Our courses center around primary texts supplemented by secondary literature. We think it is important that you spend time with the philosopher's own text and become equipped to make up your own mind regarding their ideas."
-                            maskType="diamond"
-                            imgUrl="/static/images/books.webp"
-                        />
-                        <InfoCard
-                            title="Sharpen Your Critical Thinking: Unleash Your Philosophical Potential"
-                            text="They who do not understand the past cannot comprehend the future. Philsophy is not about the past but the discovery of futures. Learn to think critically, analyze arguments, and form your own well-reasoned opinions - skills valuable in all aspects of life."
-                            maskType="diamond"
-                            imgUrl="/static/images/eye.webp"
-                        />
-                    </div>
-                </FadeIn>
-                <Heading as="h3" additionalClasses="max-w-lg">
-                    Join the <span className="text-[#6b0072]">community</span>
-                </Heading>
-                <p className="text-lg text-slate-500 py-2">
-                    What do our students say about our courses and instructors?
-                </p>
-                <Reviews />
-
-                {/* // TODO delete */}
-                <div className="flex flex-col justify-front items-center gap-4">
-                    <LoremExample />
-                    <LoremExample />
-                    <LoremExample />
-                    <LoremExample />
-                    <LoremExample />
-                    <LoremExample />
-                </div>
+                <OpeningDescription />
+                <InfoCards />
+                <Community />
+                <VisitCourses />
+                <div className="h-20" />
             </PageWrapper>
         </>
     );
 }
 
-const LoremExample = () => {
+function Hero() {
     return (
-        <p>
-            On the other hand, we denounce with righteous indignation and
-            dislike men who are so beguiled and demoralized by the charms of
-            pleasure of the moment, so blinded by desire, that they cannot
-            foresee the pain and trouble that are bound to ensue; and equal
-            blame belongs to those who fail in their duty through weakness of
-            will, which is the same as saying through shrinking from toil and
-            pain. These cases are perfectly simple and easy to distinguish. In a
-            free hour, when our power of choice is untrammelled and when nothing
-            prevents our being able to do what we like best, every pleasure is
-            to be welcomed and every pain avoided. But in certain circumstances
-            and owing to the claims of duty or the obligations of business it
-            will frequently occur that pleasures have to be repudiated and
-            annoyances accepted. The wise man therefore always holds in these
-            matters to this principle of selection: he rejects pleasures to
-            secure other greater pleasures, or else he endures pains to avoid
-            worse pains.
-        </p>
+        <div className="h-[45rem] -my-36 md:my-6 w-full bg-white flex flex-col items-center justify-center overflow-hidden rounded-md ">
+            <div className="absolute overflow-hidden -translate-y-5">
+                <FadeIn>
+                    <PolyRhythmicSpiral />
+                </FadeIn>
+            </div>
+            <div className="translate-y-2 md:translate-y-1 lg:-translate-y-1 flex flex-col items-center justify-center">
+                <div className="font-bold text-center relative z-20 bg-clip-text tracking-widest text-transparent">
+                    <Heading replacementClasses="pb-4 text-transparent bg-gradient-to-b from-black/80 to-black dark:from-white dark:to-[#AAAAAA]">
+                        Discover & Reflect
+                    </Heading>
+                </div>
+                <h3 className="text-xl my-1.5 md:text-2xl lg:text-3xl text-stone-600 font-light inter-var text-center">
+                    In-depth learning courses on philosophy
+                </h3>
+                <div className="w-[40rem] h-40 relative">
+                    {/* Gradients */}
+                    <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-[2px] w-3/4 blur-sm" />
+                    <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-px w-3/4" />
+                    <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-pink-400 to-transparent h-[5px] w-1/4 blur-sm" />
+                    <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-pink-400 to-transparent h-px w-1/4" />
+                </div>
+            </div>
+        </div>
     );
-};
+}
+
+function OpeningDescription() {
+    return (
+        <FadeIn>
+            <Heading as="h3" additionalClasses="max-w-lg">
+                <span className="text-[#6b0072]">Symposia</span> is an online
+                courseplatform that offers on-demand digital courses, live
+                university-grade seminars and personalized tuition.
+            </Heading>
+        </FadeIn>
+    );
+}
+
+function InfoCards() {
+    return (
+        <FadeIn>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full py-10">
+                <InfoCard
+                    title="Deepen Your Thinking: Explore Philosophy In-Depth"
+                    text="Dive into the world's greatest ideas with our comprehensive online philosophy courses. Go beyond introductory lectures and engage with challenging concepts in a supportive learning environment."
+                    maskType="diamond"
+                    imgUrl="/static/images/sun.webp"
+                />
+                <InfoCard
+                    title="Choose Your Learning Style: Digital, with Seminars, or 1-on-1 Dialogue"
+                    text="We offer a variety of learning formats to suit your ambition. Take courses entirely on-demand with digital content at your own pace, participate in live interactive seminars with fellow peers, or get personalized guidance with private sessions from our instructors."
+                    maskType="diamond"
+                    imgUrl="/static/images/discussion.webp"
+                />
+                <InfoCard
+                    title="Focus on Text: Meet the Greatest Minds of History"
+                    text="Gain insights from renowned philosophers by meeting them on their own ground, namely, their text! Our courses center around primary texts supplemented by secondary literature. We think it is important that you spend time with the philosopher's own text and become equipped to make up your own mind regarding their ideas."
+                    maskType="diamond"
+                    imgUrl="/static/images/books.webp"
+                />
+                <InfoCard
+                    title="Sharpen Your Critical Thinking: Unleash Your Philosophical Potential"
+                    text="They who do not understand the past cannot comprehend the future. Philsophy is not about the past but the discovery of futures. Learn to think critically, analyze arguments, and form your own well-reasoned opinions - skills valuable in all aspects of life."
+                    maskType="diamond"
+                    imgUrl="/static/images/eye.webp"
+                />
+            </div>
+        </FadeIn>
+    );
+}
+
+function Community() {
+    return (
+        <>
+            <Heading as="h3" additionalClasses="max-w-lg mt-10 md:mt-32">
+                Join the <span className="text-[#6b0072]">community</span>
+            </Heading>
+            <p className="text-lg text-slate-500 py-2">
+                What do our students say about our courses and instructors?
+            </p>
+            <Reviews />
+        </>
+    );
+}
+
+function VisitCourses() {
+    return (
+        <>
+            <Heading as="h3" additionalClasses="max-w-xl mt-10 md:mt-32">
+                Look through the eyes of{" "}
+                <span className="text-[#6b0072]">millenia</span>
+            </Heading>
+            <p className="text-lg text-slate-500 py-4">
+                Discover and partake in philosophical discourses that stretches
+                beyond empires, generations and time.
+            </p>
+            <Link href="/courses">
+                <button className="btn btn-primary">See our courses</button>
+            </Link>
+        </>
+    );
+}
