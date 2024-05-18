7f06cf7a397147b52ee2595f63d65411e6786f83
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-05-19 00:16:49 +0200

    fix: corrupted bucket keys during production

--- src/server/bucket.ts
+++ src/server/bucket.ts
@@ -1,19 +1,9 @@
 import { Storage } from "@google-cloud/storage";
 import { env } from "process";
 
-const creds = {
-    type: "service_account",
-    project_id: env.GCP_PROJECT_ID,
-    private_key_id: env.GCP_PRIVATE_KEY_ID,
-    private_key: env.GCP_PRIVATE_KEY,
-    client_email: env.GCP_CLIENT_EMAIL,
-    client_id: env.GCP_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: env.GCP_CLIENT_X509_CERT_URL,
-    universe_domain: "googleapis.com",
-};
+const rawKey = env.GCP_BUCKET_HANDLER_KEY ?? "invalid";
+
+const creds = JSON.parse(Buffer.from(rawKey, "base64").toString());
 
 const storage = new Storage({
     projectId: env.GCP_PROJECT_ID,
