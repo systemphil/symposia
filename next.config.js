02d274f8259fc83bdc17f15f2390962b114a2fdf
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-05-18 14:59:44 +0200

    feat: ci-cd dev

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-                port: '',
+                protocol: "https",
+                hostname: "storage.googleapis.com",
+                port: "",
                 pathname: `/${process.env.GCP_SECONDARY_BUCKET_NAME}/**`,
             },
         ],
     },
-}
+    webpack: (config, { isServer }) => {
+        config.resolve.alias["@"] = path.join(__dirname, "src");
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
