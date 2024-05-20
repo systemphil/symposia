c56283707fff3d8f290475bdb4d7050a03c02dd1
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-05-20 17:34:41 +0200

    test: troubleshooting5

--- src/server/bucket.ts
+++ src/server/bucket.ts
@@ -1,18 +1,18 @@
 import { Storage } from "@google-cloud/storage";
 import { env } from "process";
 
-const rawKey = env.GCP_BUCKET_HANDLER_KEY ?? "";
+// const rawKey = env.GCP_BUCKET_HANDLER_KEY ?? "";
 
-const creds = rawKey
-    ? JSON.parse(Buffer.from(rawKey, "base64").toString())
-    : {};
+// const creds = rawKey
+//     ? JSON.parse(Buffer.from(rawKey, "base64").toString())
+//     : {};
 
-const storage = new Storage({
-    projectId: creds.project_id,
-    credentials: creds,
-});
+// const storage = new Storage({
+//     projectId: creds.project_id,
+//     credentials: creds,
+// });
 
-// const storage = new Storage();
+const storage = new Storage();
 
 const PRIMARY_BUCKET_NAME = env.GCP_PRIMARY_BUCKET_NAME ?? "invalid";
 const SECONDARY_BUCKET_NAME = env.GCP_SECONDARY_BUCKET_NAME ?? "invalid";
@@ -25,7 +25,6 @@ export const primaryBucket = storage.bucket(PRIMARY_BUCKET_NAME);
 export const secondaryBucket = storage.bucket(SECONDARY_BUCKET_NAME);
 
 export async function TestBucket() {
-    console.log("rawKey", rawKey);
     const sa = await storage.getProjectId();
     console.log(sa);
     const a = await storage.getServiceAccount();
